90e69a551f29aeefbc4a6b5131e6c949c9e2e246
Piotr Szczypior <piotrekszczypior1@gmail.com>
2024-04-08 22:32:35 +0200

    Ekg100 fast fourier transform

--- src/ifft.py
+++ src/ifft.py
@@ -1,50 +1,83 @@
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-ecg100 = pd.read_csv('../data/ekg100.txt', names=['values'])
-sampling_frequency = 360
-
-ecg100['time'] = ecg100['values'].index / sampling_frequency
-
-plt.figure(figsize=(14, 8))
-plt.plot(ecg100['time'], ecg100['values'])
-plt.title('Widmo amplitudowe sygnałów')
-plt.xlabel('Czas [s]')
-plt.ylabel('Sygnal')
-plt.xlim(300, 302)
-plt.ylim(-1, 1.2)
-plt.grid(True)
-plt.legend()
-plt.show()
+SAMPLING_FREQUENCY = 360
+FILE_PATH = '../data/ekg100.txt'
 
 
-fft = np.fft.fft(ecg100['values'])
-freq_spectral = np.fft.fftfreq(ecg100['values'].size, d=1/sampling_frequency)
-aplitude_spectral = np.abs(fft)[:len(fft)//2] / (len(fft) // 2)
-freqs_positive = freq_spectral[:len(freq_spectral)//2]
-
-plt.figure(figsize=(14, 8))
-plt.plot(freqs_positive, aplitude_spectral)
-plt.title('Widmo amplitudowe sygnału ekg100')
-plt.xlabel('Częstotliwość [Hz]')
-plt.xlim(0, sampling_frequency/2)
-plt.ylim(0, 0.015)
-plt.ylabel('Amplituda')
-plt.grid(True)
-plt.legend()
-plt.show()
+def read_ecg100_signal():
+    ecg100 = pd.read_csv(FILE_PATH, names=['values'])
+    ecg100['time'] = ecg100['values'].index / SAMPLING_FREQUENCY
+    return ecg100
 
 
-inverse_fft = np.fft.ifft(fft)
-difference = ecg100['values'] - inverse_fft.real
+def calculate_fft(ecg100):
+    fft = np.fft.fft(ecg100['values'])
+    freq_spectral = np.fft.fftfreq(ecg100['values'].size, 1 / SAMPLING_FREQUENCY)
+    amplitude_spectrum = np.abs(fft)[:len(freq_spectral) // 2] // 2
+    freqs_positive = freq_spectral[:len(freq_spectral) // 2]
+    return amplitude_spectrum, freqs_positive, fft
 
-plt.figure(figsize=(14, 8))
-plt.plot(ecg100['time'], difference, label='Różnica między sygnałem oryginalnym a odtworzonym')
-plt.title('Różnica między sygnałem oryginalnym a odtworzonym z IDFT')
-plt.xlabel('Czas [s]')
-plt.ylabel('Amplituda różnicy')
-plt.grid(True)
-plt.legend()
-plt.show()
+
+def plot(values, arguments, x_lim, y_lim):
+    plt.figure(figsize=(14, 8))
+    plt.legend()
+    plt.grid(True)
+    plt.plot(arguments, values)
+    plt.xlim(300, 302)
+    plt.ylim(-1, 1.2)
+
+
+def compare_ifft(original_signal, ifft):
+    plt.figure(figsize=(10, 4))
+    plt.plot(original_signal['values'][:1000], label='Original Signal')
+    plt.plot(ifft[:1000], label='IFFT Signal', linestyle='--')
+    plt.legend()
+    plt.title('Comparison of Original and IFFT Signals')
+    plt.grid(True)
+
+
+
+def main():
+    ecg100 = read_ecg100_signal()
+    amplitude_spectrum, freqs_positive, fft = calculate_fft(ecg100)
+    inverse_fft = np.fft.ifft(fft)
+    difference = ecg100['values'] - inverse_fft.real
+
+    plt.figure(figsize=(14, 8))
+    plt.plot(ecg100['time'], ecg100['values'])
+    plt.title('Sygnał ekg100.txt')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Sygnal')
+    plt.xlim(300, 302)
+    plt.ylim(-1, 1.2)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    # plt.figure(figsize=(14, 8))
+    # plt.plot(freqs_positive, amplitude_spectrum)
+    # plt.title('Widmo amplitudowe sygnału ekg100')
+    # plt.xlim(0, SAMPLING_FREQUENCY / 2)
+    # plt.ylim(0, 0.015)
+    # plt.ylabel('Amplituda')
+    # plt.xlabel('Częstotliwość [Hz]')
+    # plt.grid(True)
+    # plt.legend()
+    # plt.show()
+
+    compare_ifft(ecg100, inverse_fft)
+
+    plt.figure(figsize=(14, 8))
+    plt.plot(ecg100['time'], difference, label='Różnica między sygnałem oryginalnym a odtworzonym')
+    plt.title('Różnica między sygnałem oryginalnym a odtworzonym z IDFT')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Amplituda różnicy')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
