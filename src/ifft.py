f56975cfa679411d643b6a1ce524a2517f9a0f47
Piotr Szczypior <piotrekszczypior1@gmail.com>
2024-04-09 23:36:34 +0200

    Aplitude spectrum in EKG Signal

--- src/ifft.py
+++ src/ifft.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os 
 
 SAMPLING_FREQUENCY = 360
 FILE_PATH = '../data/ekg100.txt'
+DIRECTORY = "images"
 
 
 def read_ecg100_signal():
@@ -13,6 +15,8 @@ def read_ecg100_signal():
 
 
 def calculate_fft(ecg100):
+    average_signal_value = np.mean(ecg100['values'])
+    ecg100['values'] = ecg100['values'] - average_signal_value
     fft = np.fft.fft(ecg100['values'])
     freq_spectral = np.fft.fftfreq(ecg100['values'].size, 1 / SAMPLING_FREQUENCY)
     amplitude_spectrum = np.abs(fft)[:len(freq_spectral) // 2] // 2
@@ -29,14 +33,18 @@ def plot(values, arguments, x_lim, y_lim):
     plt.ylim(-1, 1.2)
 
 
-def compare_ifft(original_signal, ifft):
+def compare_ifft_on_plot(original_signal, ifft):
     plt.figure(figsize=(10, 4))
     plt.plot(original_signal['values'][:1000], label='Original Signal')
     plt.plot(ifft[:1000], label='IFFT Signal', linestyle='--')
     plt.legend()
+    plt.xlabel('Próbki')
+    plt.ylabel('Sygnal [mV]')
     plt.title('Comparison of Original and IFFT Signals')
     plt.grid(True)
-
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    plt.savefig(os.path.join(DIRECTORY, "orignal_ifft_comparison"))
 
 
 def main():
@@ -49,34 +57,39 @@ def main():
     plt.plot(ecg100['time'], ecg100['values'])
     plt.title('Sygnał ekg100.txt')
     plt.xlabel('Czas [s]')
-    plt.ylabel('Sygnal')
+    plt.ylabel('Sygnal [mV]')
     plt.xlim(300, 302)
     plt.ylim(-1, 1.2)
     plt.grid(True)
     plt.legend()
-    plt.show()
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    plt.savefig(os.path.join(DIRECTORY, "ekg100_signal_plot"))
 
-    # plt.figure(figsize=(14, 8))
-    # plt.plot(freqs_positive, amplitude_spectrum)
-    # plt.title('Widmo amplitudowe sygnału ekg100')
-    # plt.xlim(0, SAMPLING_FREQUENCY / 2)
-    # plt.ylim(0, 0.015)
-    # plt.ylabel('Amplituda')
-    # plt.xlabel('Częstotliwość [Hz]')
-    # plt.grid(True)
-    # plt.legend()
-    # plt.show()
+    plt.figure(figsize=(14, 8))
+    plt.plot(freqs_positive, amplitude_spectrum)
+    plt.title('Widmo amplitudowe sygnału ekg100')
+    plt.ylabel('Amplituda')
+    plt.xlabel('Częstotliwość [Hz]')
+    plt.grid(True)
+    plt.legend()
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    plt.savefig(os.path.join(DIRECTORY, "ekg100_amplitude_spectrum"))
 
-    compare_ifft(ecg100, inverse_fft)
+    compare_ifft_on_plot(ecg100, inverse_fft)
 
     plt.figure(figsize=(14, 8))
     plt.plot(ecg100['time'], difference, label='Różnica między sygnałem oryginalnym a odtworzonym')
-    plt.title('Różnica między sygnałem oryginalnym a odtworzonym z IDFT')
+    plt.title('Różnica między sygnałem oryginalnym a odtworzonym z IFFT')
     plt.xlabel('Czas [s]')
-    plt.ylabel('Amplituda różnicy')
+    plt.ylabel('Różnica [mV]')
     plt.grid(True)
+    plt.xlim(300, 302)
     plt.legend()
-    plt.show()
+    if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
+    plt.savefig(os.path.join(DIRECTORY, "original_ifft_diffrence"))
 
 
 if __name__ == '__main__':
